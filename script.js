982669f0bfd3d72ac0ec242a22929152d4d1dc7a
Akash Aware <akashaware90@gmail.com>
2025-03-06 14:31:07 +0530

    Add Some Js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//******************************* Easy Question *******************************//
+
+
 //Q1. Write a function that takes an integer minutes and converts it to seconds
 
 function minutestoSeconds(minutes) {
@@ -51,7 +54,38 @@ console.log(Sum2Number(60, 50)) //this condition is false because 60 + 50 is gre
 function evenOrOdd(num) {
     return num % 2 === 0 ? "Even" : "Odd"; // using ternary operator 
 }
-
 console.log(evenOrOdd(4)); // even
 console.log(evenOrOdd(7)); // odd
 
+
+
+//******************************* Meadium Question *******************************//
+
+
+//Q1.Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10
+//formula ---> sum = n * (n + 1) / 2
+
+function sumNumbers(n) {
+    return (n * (n + 1)) / 2;
+}
+console.log(sumNumbers(4));
+
+
+//Q2. Write a function that takes three numbers and returns the largest of them.
+
+function findLargest(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(findLargest(22, 95, 50));
+
+
+//Q3.Write a function that returns the factorial of a given number.
+
+function factorial(n) {
+    let result = 1;  // start with 1
+    for (let i = 2; i <= n; i++) {  // loop from 2 to n
+        result *= i; //  multiply result by i each time
+    }
+    return result;
+}
+console.log(factorial(5))
